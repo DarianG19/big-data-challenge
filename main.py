645322ce73d8e1ebd62a6186bad64af3eb33ffd3
Darian Gerent <darian.gerent19@gmail.com>
2024-01-31 19:58:44 +0100

    feat: iterate through all files and print datasets

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from readFiles import read_files
 from readFiles import createDiagram
 
 if __name__ == '__main__':
-     # TODO: Muss durch Ordner-Pfad geändert werden
-     read_files('./dataset/0a7e8940-9008-45fa-9a99-054757404083.h5')
+     read_files('./dataset')
 
      # createDiagram(graph_data[0], graph_data[1])
--- readFiles.py
+++ readFiles.py
@@ -1,27 +1,40 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from datetime import datetime
 
 
 def read_files(path):
     """
     Funktion zum Einlesen der Dateien in einem bestimmten Pfad.
-    TODO: Aktuell wird ein Pfad zu einer Datei angegeben.
     :param path: Der Pfad zum jeweiligen Ordner
     :return:
     """
-    f = h5py.File('{}'.format(path), 'r')
-    list(f.keys())
-    dsetGroup = f['data']
-    group_x = np.arange(1, 1001)
-    dset_list = []
-    for dset in dsetGroup:
-        dset_list.append(dset)
-        group_y = np.array(dsetGroup[dset])
 
-        createDiagram(group_x, group_y)
-    print(dset_list)
+    # Iterate through files in folder
+    for file_name in os.listdir(path):
+        file_path = os.path.join(path, file_name)
+
+        # Check, if file_path is a file and not a folder
+        if os.path.isfile(file_path):
+
+            try:
+                f = h5py.File('{}'.format(file_path), 'r')
+                list(f.keys())
+                dsetGroup = f['data']
+                # group_x = np.arange(1, 1001)
+                dset_list = []
+                for dset in dsetGroup:
+                    dset_list.append(dset)
+                    # group_y = np.array(dsetGroup[dset])
+
+                print(dset_list)
+            except Exception as e: # TODO: Aktuell werden jegliche Fehler abgefangen, muss spezifischer werden, um "fehlerhafte" Dateien/Daten dennoch auszulesen
+                print("Allgemeiner Fehler")
+                continue
+        else:
+            print("{} skipped ... ".format(file_path))
 
 
 def createDiagram(x_array, y_array):
