b0df5bd230678de896d7f9975ae480a7d76d4fa2
noku <noah.kuse@hs-osnabrueck.de>
2024-02-18 14:34:58 +0100

    fix: change way error messages are printed

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from readFiles import read_files
 from readFiles import createDiagram
+from pymongo import MongoClient
 
-if __name__ == '__main__':
-     read_files('./dataset')
+mongo_uri = "mongodb+srv://<noahkuse>:<BigDMitBigD>@bigdataproject.f6aka7m.mongodb.net/"
+client = MongoClient(mongo_uri)
+db = client.bigdataproject  # Ändere 'mydatabase' entsprechend deiner Datenbank
 
-     # createDiagram(graph_data[0], graph_data[1])
+try:
+    # Einfache Abfrage, um sicherzustellen, dass die Verbindung funktioniert
+    result = db.mycollection.find_one()
+    print("Verbindung erfolgreich. Beispiel-Dokument:", result)
+except Exception as e:
+    print(f"Fehler bei der Datenbankabfrage: {e}")
+
+
+#if __name__ == '__main__':
+     #read_files('./dataset/dataset')
+
+     #createDiagram(graph_data[0], graph_data[1])
