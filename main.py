1f7049a8293612925eb6d64e82c01e89073d55b9
Darian Gerent <darian.gerent19@gmail.com>
2024-03-18 16:44:19 +0100

    fix: bug opening files

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from mongo_db import get_data_for_specific_region_and_instrument
 
 def main():
     # Lokale Dateien lesen und formatieren etc.
-    # read_and_store_locale_files('./dataset')
+    read_and_store_locale_files('./dataset')
 
     # Mit korrigierten Daten aus DB arbeiten
-    data = get_data_for_specific_region_and_instrument('europe', 'unicorn')
-    for entry in data:
-        print(entry)
+    # data = get_data_for_specific_region_and_instrument('europe', 'unicorn')
+    # for entry in data:
+    #     print(entry)
 
 
 if __name__ == '__main__':
